8bbedb304824ba4be8bdfe1b7119c6e1fa5a5349
Paulo <paulo.100potter@gmail.com>
2024-08-08 20:56:54 -0300

    feat: Criação da entidade Session

---
+++ src/main/kotlin/com/example/venda_ingressos/entities/Session.kt
@@ -0,0 +1,19 @@
+package com.example.venda_ingressos.entities
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@Entity
+data class Session(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: UUID? = null,
+    val name: String,
+    val quantityTickets: Int,
+    val datePresentation: LocalDate
+)
