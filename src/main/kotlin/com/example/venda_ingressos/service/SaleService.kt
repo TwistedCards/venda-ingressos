6579830089f2766a64038671f1b135d9a5b34efa
Paulo <paulo.100potter@gmail.com>
2024-08-08 22:06:59 -0300

    feat: Regra de negócio no SaleService

--- src/main/kotlin/com/example/venda_ingressos/service/SaleService.kt
+++ src/main/kotlin/com/example/venda_ingressos/service/SaleService.kt
@@ -1,23 +1,47 @@
 package com.example.venda_ingressos.service
 
+import com.example.venda_ingressos.dto.SaleDTO
 import com.example.venda_ingressos.entities.Sale
+import com.example.venda_ingressos.repository.SaleRepository
+import com.example.venda_ingressos.repository.SessionRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.util.Random
 
 @Service
-class SaleService {
+class SaleService(
+    val repository: SaleRepository,
+    val sessionRepository: SessionRepository
+) {
 
-    fun generateSale(client: String, numberOfTickets: Int): Sale {
-        var operacao: Long = 0
+    // todo inserir o transactional dps
+    fun generateSale(saleDTO: SaleDTO): Sale {
         val totalValue = BigDecimal.valueOf(500)
+        val sessionEntity = sessionRepository.findByName(saleDTO.nameSession)
 
-        return Sale(
-            operation = operacao++,
-            client = client,
-            numberOfTickets = numberOfTickets,
-            totalValue = totalValue.multiply(BigDecimal.valueOf(numberOfTickets.toLong()))
+        // todo Melhorar isso mais pra frente
+        if (sessionEntity.quantityTickets == 0) {
+            throw Exception("Ingressos esgotados")
+        }
+
+        sessionEntity.quantityTickets -= saleDTO.numberOfTickets
+
+        sessionRepository.save(sessionEntity)
+
+        val saleEntity = Sale(
+            nameOfSession = sessionEntity.name,
+//            client = "Teste cliente",
+            numberOfTickets = saleDTO.numberOfTickets,
+            totalValue = totalValue.multiply(BigDecimal.valueOf(saleDTO.numberOfTickets.toLong())),
+            session = sessionEntity
         )
+
+        return repository.save(saleEntity)
+    }
+
+    fun getAll(): List<Sale> {
+        return repository.findAll()
     }
 
 }
