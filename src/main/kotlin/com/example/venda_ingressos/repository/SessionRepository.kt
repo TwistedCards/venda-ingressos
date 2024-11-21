bf25384364402c77e4768d4a72579ddd098c6b8a
Paulo <paulo.100potter@gmail.com>
2024-11-21 15:47:37 -0300

    feat: Novo metodo no SessionRepository e nova regra de negócio no sessionService

--- src/main/kotlin/com/example/venda_ingressos/repository/SessionRepository.kt
+++ src/main/kotlin/com/example/venda_ingressos/repository/SessionRepository.kt
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface SessionRepository : JpaRepository<SessionEntity, UUID> {
+
+    fun findByMovieId(id: UUID): List<SessionEntity>? = null
+
 }
--- src/main/kotlin/com/example/venda_ingressos/service/SessionService.kt
+++ src/main/kotlin/com/example/venda_ingressos/service/SessionService.kt
@@ -9,6 +9,7 @@ import com.example.venda_ingressos.exceptions.IllegalArgumentException
 import com.example.venda_ingressos.mapper.SessionMapper
 import com.example.venda_ingressos.repository.*
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 import java.util.*
 
 @Service
@@ -20,6 +21,20 @@ class SessionService(
 ) {
 
     fun save(request: SessionRequest): SessionResponse {
+        val listEntity = repository.findByMovieId(request.idMovie)
+
+        if (!listEntity.isNullOrEmpty()) {
+            listEntity.forEach {
+                val date = LocalDateTime.parse(request.startTime)
+                if (it.startTime == date && it.room.id == request.idRoom) {
+                    throw IllegalArgumentException(
+                        "m=save, msg=Já existe uma sessão salva na data e hora '$date' " +
+                                "na sala '${it.room.roomName}'"
+                    )
+                }
+            }
+        }
+
         val movieEntity = movieRepository.findById(request.idMovie).orElseThrow {
             EntityNotFoundException("O filme com id ${request.idMovie} não foi encontrado.")
         }
